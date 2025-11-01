fdbec918f2c1192d804efa4101bc0f0f70babc4d
narayanbehera14 <beheranarayancs222330@gmail.com>
2025-11-01 23:55:21 +0530

    updated

---
+++ Array/Array_Question.java
@@ -0,0 +1,15 @@
+// Print all element in Array
+package DSA_JAVA_PYTHON ;
+
+public class Array_Question  {
+
+public static void main (String []arg) {
+
+int[] arr = {1,2,5,8} ;
+for (int i = 0; i < arr.length; i++) {
+    System.out.println(arr[i]);
+    
+}
+
+}
+}
