33786df04eb7c7338229903dde4bdeb0e66973a6
narayanbehera14 <beheranarayancs222330@gmail.com>
2026-02-09 22:52:59 +0530

    updated

---
+++ String_python/day1.py
@@ -0,0 +1,27 @@
+s1 = 'GFG'
+s2 = "GFG"
+print(s1)
+print(s2)
+
+s = """I am Learning
+Python String on GeeksForGeeks"""
+print(s)
+
+s = '''I'm a 
+Geek'''
+print(s)
+
+s = "GeeksForGeeks"
+print(s[0])
+print(s[4])
+
+s = "GeeksforGeeks"
+print(s[-10])
+print(s[-5])
+
+s = "GeeksforGeeks"
+print(s[1:4])
+print(s[:3])
+print(s[3:])
+print(s[::-1])
+
