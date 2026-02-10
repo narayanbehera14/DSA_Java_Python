696ec9e1c57f88cec26ae85e5dd043698aefc682
narayanbehera14 <beheranarayancs222330@gmail.com>
2026-02-10 23:30:15 +0530

    updated

--- String_python/day1.py
+++ String_python/day1.py
@@ -25,3 +25,54 @@ print(s[:3])
 print(s[3:])
 print(s[::-1])
 
+s = "python"
+for char in s:
+    print(char)
+
+
+s = "geeksforGeeks"
+s ="G" + s[1:]
+print(s)
+
+
+s = "GFG"
+del s
+
+s = "hello geeks"
+s1 = "H" + s[1:]
+s2 = s.replace("geeks", "GeeksforGeeks")
+print(s1)
+print(s2)
+
+s = "GeeksforGeeks"
+print(len(s))
+
+s = "Hello World"
+print(s.upper())
+print(s.lower())
+
+s = "   GFG   "
+print(s.strip())
+
+s = "python is fun"
+print(s.replace("fun","awsome"))
+
+s1 = "Hello"
+s2 = "World"
+print(s1 + " " + s2)
+
+s = "Hello"
+print(s * 3)
+
+name = "Alice"
+age = 22
+print(f"Name: {name} , Age:{age}")
+
+
+s = "My name is {} and I am {} year old.".format("Alice",22)
+print(s)
+
+
+s = "GeeksforGeeks"
+print("Geeks"in s)
+print("GFG" in s)
