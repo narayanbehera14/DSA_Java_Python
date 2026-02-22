55fe1d870d4a525154a5bf92b4303f7be324a6eb
narayanbehera14 <beheranarayancs222330@gmail.com>
2026-02-22 23:59:26 +0530

    updated

---
+++ modules/area.py
@@ -0,0 +1,18 @@
+def circle(radius:float):
+    area = 3.14 * radius * radius
+    print(f"Area of circle with radius {radius} = {area}")
+
+
+def rectangle(length:float , breadth:float) -> None:
+    area = length * breadth
+    print(f"Area of rectangle = {area}")
+
+def triangle(base:float,height:float) -> None:
+    area = 0.5 * height * base
+    print(f"Area of triangle = {area}")
+
+
+
+circle(56.996)
+rectangle(25,41)
+triangle(44,55)
--- oops/bank.py
+++ oops/bank.py
@@ -23,10 +23,12 @@ class Bank:
         else:
             #self.balance -= amount
             self.balance = self.balance - amount
+            print("Withdrawal sucessful")
     
     def deposit(self) ->None:
         amount = int(input("Enter amount to deposit = "))
         self.balance = self.balance + amount
+        print("Deposit successfull..!")
 
 # b1 = Bank()
 # b1.show_balanced()
@@ -48,11 +50,22 @@ class Bank:
 # Banks[0].deposit()
 # Banks[0].show_balanced()
 Banks = []
+
+def check_account_exists(acc_no: int):
+    global Banks
+    for obj in Banks:
+        if obj.account == acc_no:
+            return obj
+    return None
+
+
 while True:
-    print("1 . Create account:")
+    print("1. Create account:")
     print("2. Show all bank details")
     print("3. Deposit amount")
-    print("4.Exit")
+    print("4. withdraw amount")
+    print("5. Transfer amount")
+    print("6. Exit")
 
     choice = int(input("Enter choice = "))
 
@@ -73,15 +86,57 @@ while True:
             print("No account have been created yet..")
         else:
             acc_no = int(input("Enter account number to deposit = "))
+            found = False
+
+
             for obj in Banks:
                 if obj.account == acc_no:
                     obj.deposit()
+                    found = True
                     break
-
+            
+            if not found:
+                print("Account does not exist")
 
     elif choice == 4:
+        if len(Banks) == 0:
+            print("No account have been created yet..")
+        else:
+            acc_no = int(input("Enter account number to withdraw = "))
+            found = False
+
+            for obj in Banks:
+                if obj.account == acc_no:
+                        
+                        obj.withdraw()
+                        found = True
+                        break
+
+            if not found:
+
+                print("Account does not exist")
+
+
+    elif choice == 5:
+        from_acc_no = int(input("Enter account number from which you want to transfer = "))
+        to_acc_no = int(input("Enter account number to which you want to transfer = "))
+        from_acc_obj = check_account_exists(from_acc_no)
+        to_acc_obj  = check_account_exists(to_acc_no)
+
+        if from_acc_obj != None and to_acc_obj != None:
+            transfer_amount = int(input("Enter transfer amount = "))
+            if from_acc_obj.balance < transfer_amount:
+                print("Insuffcient balance")
+            else:
+                from_acc_obj.balance -= transfer_amount
+                to_acc_obj.balance += transfer_amount
+
+
+        else:
+            print("Account does not exists")
+
+    elif choice == 6:
         break
-        
 
     else:
         print("Invalid Choice :")
