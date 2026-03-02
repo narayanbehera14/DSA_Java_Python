2b31d90b03c03512a48b82640316b1533cd2d959
narayanbehera14 <beheranarayancs222330@gmail.com>
2026-03-02 23:56:41 +0530

    updated

---
+++ C++_DSA/basic.cpp
@@ -0,0 +1,110 @@
+// #include <iostream>
+// using namespace std;
+
+// int main() {
+//     cout << "Hello world!";
+
+//     return 0 ;
+
+// }
+
+// #include <iostream>
+
+// int main(){
+//     std::cout << "Hello Narayan";
+//     return 0;
+// }
+
+// #include <iostream>
+// using namespace std;
+
+// int main() {
+//     cout << "Hello World ";
+//     cout << "Have a great day!";
+
+//     return 0;
+// }
+
+// #include <iostream>
+// using namespace std;
+
+// int main() {
+//     cout << " Hello Narayan";
+//     cout << "I am learning C++";
+//     return 0 ;
+// }
+
+// #include <iostream>
+// using namespace std;
+
+// int main () {
+//     cout << 3 + 3 ;
+//     cout << 3*4 ;
+//     return 0;
+
+// }
+
+// #include <iostream>
+// using namespace std;
+
+// int main() {
+
+//     cout << "Hello World!" << "\n";
+//     cout << "I am learnign C++";
+//     return 0;
+// }
+
+// #include <iostream>
+// using namespace std;
+
+// int main(){
+//     cout << "Hello world!" <<"\n\n";
+//     cout << "I am learning c ++ for DSA";
+//     return 0 ;
+
+// }
+
+// #include <iostream>
+// using namespace std;
+
+// int main() {
+//     cout << "Narayan Behera" << endl;
+//     cout <<"amity universiyt mumbai";
+//     return 0;
+// }
+
+// #include <iostream>
+// using namespace std;
+
+// int main() {
+//     int myNum = 78;
+//     myNum = 55;
+//     char myname = 'L';
+//     string myText = "hello";
+//     bool myBoolean = true ;
+//     cout << myBoolean << endl;
+//     cout << myNum;
+//     cout << myname << endl;
+//     cout <<myText << endl;
+//     return 0;
+
+// }
+
+#include <iostream>
+using namespace std;
+
+int main() {
+    int myAge = 65;
+    string name = "Narayan";
+    double height = 6.6;
+
+    int x = 4;
+    int y = 5;
+    int sum = x + y ;
+
+    cout << sum << endl;
+    cout << " is " << name << " and his age " << height <<endl;
+    cout << " I am " << myAge << " year old. ";
+    
+    return 0;
+}
---
+++ C++_DSA/variable.cpp
@@ -0,0 +1,22 @@
+// #include <iostream>
+// using namespace std;
+
+// int main(){
+//     int x = 5, y = 8,z = 95;
+//     int sum = x + y + z;
+//     cout << sum ; 
+//     return 0 ;
+// }
+
+// #include <iostream>
+// using namespace std;
+
+// int main(){
+//     int length = 4;
+//     int width = 5;
+//     int area = length * width;
+//     cout << "Lenght is : " << length << endl;
+//     cout << " width is : " << width << endl;
+//     cout << area ;
+//     return 0 ;
+// }
