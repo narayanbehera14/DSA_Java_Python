3ff5d8138f558123dcaba3f0db1d824607f0b979
narayanbehera14 <beheranarayancs222330@gmail.com>
2026-03-15 23:53:29 +0530

    updated

--- C++_DSA/fun.cpp
+++ C++_DSA/fun.cpp
@@ -1,37 +1,117 @@
+// // #include<iostream>
+// // using namespace std;
+
+// // void myfunction(){
+
+// //     cout<<"I just got executed!"<<endl;
+
+// // }
+// // void my();
+
+// // void names(){
+// //     cout<<"my name is liju:" <<endl;
+// // }
+// // int main(){
+// //     myfunction();
+// //     names();
+// //     my();
+// //     return 0;
+// // }
+
+// // void my(){
+// //     cout<<"this is a function:";
+// // }
+
 // #include<iostream>
+// #include<string>
 // using namespace std;
 
-// void myfunction(){
-
-//     cout<<"I just got executed!"<<endl;
-
+// void myfunction(string fname) {
+//     cout << fname <<"refsnes\n";
 // }
-// void my();
 
-// void names(){
-//     cout<<"my name is liju:" <<endl;
-// }
-// int main(){
-//     myfunction();
-//     names();
-//     my();
+// int main() {
+//     myfunction("liju");
+//     myfunction("jenny");
+//     myfunction("anja");    
 //     return 0;
 // }
 
-// void my(){
-//     cout<<"this is a function:";
+// #include<iostream>
+// using namespace std;
+
+// void myfunction(string country = "norway") {
+//     cout << "country name = " << country <<endl;
+
+// }
+
+// int main() {
+//     myfunction("sweden");
+//     myfunction("india");
+//     myfunction();
+//     myfunction("USA");
+//     return 0;
+// }
+
+
+// #include<iostream>
+// using namespace std;
+
+// void myfunction(string fname, int age) {
+//     cout << fname << "fname" <<age << "year old "<<endl;
+// }
+
+// int main() {
+//     myfunction("liju",5);
+//     myfunction("amar" , 22);
+//     myfunction("behera",41);
+//     return 0;
+// }
+
+// #include <iostream>
+// using namespace std;
+// int myfunction(int x) {
+//     return 5 + x ;
+
+// }
+
+// int main() {
+//     cout << " value after adding = " << myfunction(22);
+//     return 0;
+// }
+
+// #include <iostream>
+// using namespace std;
+
+// void myfunction(int x){
+//     cout << 5 + x ;
+
+// }
+// int main() {
+//     myfunction(21);
+//     return 0 ;
+// }
+
+// #include<iostream>
+// using namespace std;
+
+// void myfunction(int x , int y) {
+//     cout << x + y ;
+// }
+
+// int main() {
+//     myfunction(3 , 5);
+//     return 0;
 // }
 
 #include<iostream>
-#include<string>
 using namespace std;
 
-void myfunction(string fname) {
-    cout << fname <<"refsnes\n";
+void myfunction(int x , int y){
+    cout << x + y ;
 }
 
 int main() {
-    myfunction("liju");
-    
+    myfunction(3,4);
     return 0;
 }
